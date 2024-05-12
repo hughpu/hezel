02f81bd95d1b0c54dc68301e37ce5678e611e591
hughpu <hughpu@hotmai.com>
2024-05-12 16:38:25 +0800

    feat(event): add KeyEvents implementation

---
+++ src/hezel/event/key_event.h
@@ -0,0 +1,62 @@
+#pragma once
+
+#include "event.h"
+
+#include <sstream>
+
+namespace hezel
+{
+class KeyEvent : public Event
+{
+public:
+    int get_keycode() const
+    {
+        return m_keycode;
+    }
+    
+    EVENT_CLASS_CATEGORY(kEventCategoryKeyBoard | kEventCategoryInput)
+protected:
+    KeyEvent(int keycode) : m_keycode(keycode) {}
+    int m_keycode;
+};
+
+class KeyPressedEvent : public KeyEvent
+{
+public:
+    KeyPressedEvent(int keycode, int repeat_count)
+        : KeyEvent(keycode), m_repeat_count(repeat_count) {}
+    
+    int get_repeat_count() const { return m_repeat_count; }
+    
+    const std::string ToString() const override
+    {
+        std::stringstream ss;
+        ss << "KeyPressedEvent: " << m_keycode << " (" << m_repeat_count << ")";
+        return ss.str();
+    }
+    
+    EVENT_CLASS_TYPE(kKeyPressed);
+private:
+    int m_repeat_count;
+};
+
+class KeyReleasedEvent : public KeyEvent
+{
+public:
+    KeyReleasedEvent(int keycode)
+        : KeyEvent(keycode) {}
+    
+    int get_repeat_count() const { return m_repeat_count; }
+    
+    const std::string ToString() const override
+    {
+        std::stringstream ss;
+        ss << "KeyReleasedEvent: " << m_keycode;
+        return ss.str();
+    }
+    
+    EVENT_CLASS_TYPE(kKeyReleased);
+private:
+    int m_repeat_count;
+};
+}
